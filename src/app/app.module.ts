8bd4367190b8f73cdf63baab81d073a9bb076861
Oscar <oscar.espinozaea19@gmail.com>
2022-03-09 11:29:55 -0600

    first Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { heroesModule } from './heroes/heroes.module';
+import { contadorModule } from './contador/contador.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+ 
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    heroesModule,
+    contadorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,17 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  
+})
+export class ListadoComponent  {
+
+  heroes:string[]=['SpiderMan','Hulk','Thor','Batman','Thanos','Capitan'];
+  delete:string='';
+
+  borrarHeroe():void{
+  this.delete=this.heroes.shift() || '';
+   
+  }
+}
